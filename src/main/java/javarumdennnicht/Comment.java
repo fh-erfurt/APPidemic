896c73c2975410b42564cbb477df1f51f59afd2e
AnkaMulm <anka.h.2001@gmail.com>
2021-01-06 12:51:14 +0100

    Comment Klasse Prototyp (jetzt aber)

---
+++ src/main/java/javarumdennnicht/Comment.java
@@ -0,0 +1,43 @@
+package main;
+
+public class Comment
+{
+
+    private String text;
+    private Profile commenter;
+
+    // leerer constructor
+    public Comment()
+    {
+    }
+    // ordentlicher constructor
+    public Comment(Profile commenter, String text)
+    {
+        this.commenter = commenter;
+        this.text = text;
+    }
+
+
+
+    // Setter und Getter
+    public String getText()
+    {
+        return text;
+    }
+
+    public void setText(String text)
+    {
+        this.text = text;
+    }
+
+    public Profile getCommenter()
+    {
+        return commenter;
+    }
+
+    // brauchen wir da überhaupt nen setter?
+    public void setCommenter(Profile commenter)
+    {
+        this.commenter = commenter;
+    }
+}
