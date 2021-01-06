d00ae0cfbad54d6eb8b70b9311eca07b3b961061
HenningCV <henningvenhorst@gmail.com>
2021-01-06 12:48:16 +0100

    User Class Attribute hinzugefügt

--- src/main/java/javarumdennnicht/User.java
+++ src/main/java/javarumdennnicht/User.java
@@ -1,5 +1,12 @@
 package javarumdennnicht;
 
+import java.time.LocalDate
 public class User {
     private String username;
+    private String password;
+    private String eMail;
+    private String firstName;
+    private String lastName;
+    private LocalDate birthdate;
+    private Profile relatedProfile;
 }
