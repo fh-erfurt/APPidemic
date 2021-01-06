158a7735bff71cfd386821574b4046e63755bc97
HenningCV <henningvenhorst@gmail.com>
2021-01-06 12:56:24 +0100

    User Class Setter und Getter hinzufügen

--- src/main/java/javarumdennnicht/User.java
+++ src/main/java/javarumdennnicht/User.java
@@ -1,6 +1,7 @@
 package javarumdennnicht;
 
-import java.time.LocalDate
+import java.time.LocalDate;
+
 public class User {
     private String username;
     private String password;
@@ -9,4 +10,62 @@ public class User {
     private String lastName;
     private LocalDate birthdate;
     private Profile relatedProfile;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
+
+    public void seteMail(String eMail) {
+        this.eMail = eMail;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public Profile getRelatedProfile() {
+        return relatedProfile;
+    }
+
+    public void setRelatedProfile(Profile relatedProfile) {
+        this.relatedProfile = relatedProfile;
+    }
+
+
 }
