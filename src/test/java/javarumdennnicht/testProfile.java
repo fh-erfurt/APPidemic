08f41239b5364f96b3b17f0b94045627722fa967
diafreak <diafreak212@gmail.com>
2021-01-30 00:50:26 +0100

    first tests createAlarm()

--- src/test/java/javarumdennnicht/testProfile.java
+++ src/test/java/javarumdennnicht/testProfile.java
@@ -19,9 +19,11 @@ public class testProfile
 {
     //users used for all the tests, to prevent duplicated code they are declared here so every test can use them
     //necessary because a profile needs a user, so in order to test the Profile-functions we need users for them
-    private final User user1 = new User("hansmueller",  "12345", "hansmueller@web.de",  "Hans",   "Müller", LocalDate.of(2000,1,10));
-    private final User user2 = new User("tomvogt",      "98765", "tomvogt@web.de",      "Tom",    "Vogt",   LocalDate.of(1987,3, 4));
-    private final User user3 = new User("kalterdieter", "45454", "kalterdieter@web.de", "Dieter", "Kalt",   LocalDate.of(2001,6,13));
+    private final User user1 = new User("hansmueller",  "12345", "hansmueller@web.de",  "Hans",   "Müller", LocalDate.of(2000, 1,10));
+    private final User user2 = new User("tomvogt",      "98765", "tomvogt@web.de",      "Tom",    "Vogt",   LocalDate.of(1987, 3, 4));
+    private final User user3 = new User("kalterdieter", "45454", "kalterdieter@web.de", "Dieter", "Kalt",   LocalDate.of(2001, 6,13));
+    private final User user4 = new User("heinzlayla",   "00000", "heinzlayla@web.de",   "Layla",  "Heinz",  LocalDate.of(1999,11,22));
+    private final User user5 = new User("romystolz",    "69696", "romystolz@web.de",    "Romy",   "Stolz",  LocalDate.of(2002, 7,29));
 
 
 
@@ -130,6 +132,90 @@ public class testProfile
 
 
 
+    // ======================================================================================================================== //
+    // ================================================== TEST createAlarm() ================================================== //
+    // ======================================================================================================================== //
+    @Test
+    public void creating_an_alarm_should_have_no_duplicate_profiles_in_the_taggedProfile_list()
+    {
+        //preparation for System.out.println-check
+        //compares what is printed out from "System.out.println" with an expected string
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        //newLine contains the line-separator that "println()" creates after a line is printed
+        String newLine = System.getProperty("line.separator");
+
+
+        // GIVEN
+        Profile profilePost     = user1.getRelatedProfile();
+        Profile taggedProfile1  = user2.getRelatedProfile();    //following profilePost
+        Profile taggedProfile2  = user3.getRelatedProfile();    //following profilePost
+
+        //profiles have to follow each other in order for the alarm to trigger on their profiles
+        taggedProfile1.follow(profilePost);
+        taggedProfile2.follow(profilePost);
+        profilePost.follow(taggedProfile1);
+        profilePost.follow(taggedProfile2);
+
+        ArrayList<Profile> taggedProfiles1 = new ArrayList<>();
+        taggedProfiles1.add(taggedProfile1);
+        taggedProfiles1.add(taggedProfile2);
+
+        ArrayList<Profile> taggedProfiles2 = new ArrayList<>();
+        taggedProfiles1.add(taggedProfile2);
+
+        //make 2 posts in which taggedProfile2 is tagged twice
+        profilePost.newPost("Bild 2TaggedUsersThatFollow", "2TaggedUsers-Post", "2Tags", 2021, 1, 10, taggedProfiles1);
+        profilePost.newPost("Bild 1TaggedUserThatFollow",  "1TaggedUser-Post",  "1Tag",  2021, 1, 21, taggedProfiles2);
+
+        // WHEN
+        profilePost.createAlarm();
+
+        // THEN
+        assertEquals("The taggedProfile should not have duplicate profiles in it.",
+                     "tomvogt: ALARM" + newLine + "kalterdieter: ALARM" + newLine,
+                     outContent.toString());
+    }
+
+
+    @Test
+    public void alarm_should_trigger_on_all_tagged_profiles_of_posts_in_which_im_tagged_but_are_not_mine()
+    {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+        String newLine = System.getProperty("line.separator");
+
+
+        // GIVEN
+        Profile profilePost     = user1.getRelatedProfile();
+        Profile taggedProfile1  = user2.getRelatedProfile();
+        Profile taggedProfile2  = user3.getRelatedProfile();
+        Profile taggedProfile3  = user4.getRelatedProfile();
+
+        //profiles have to follow each other in order for the alarm to trigger on their profiles
+        taggedProfile1.follow(taggedProfile2);
+        taggedProfile3.follow(taggedProfile2);
+        taggedProfile2.follow(taggedProfile1);
+        taggedProfile2.follow(taggedProfile3);
+
+        ArrayList<Profile> taggedProfiles = new ArrayList<>();
+        taggedProfiles.add(taggedProfile1);
+        taggedProfiles.add(taggedProfile2);
+        taggedProfiles.add(taggedProfile3);
+
+        profilePost.newPost("Bild 3TaggedUsersThatFollow", "3TaggedUsers-Post", "3Tags", 2021, 1, 10, taggedProfiles);
+
+        // WHEN
+        taggedProfile2.createAlarm();
+
+        // THEN
+        assertEquals("The taggedProfile should not have duplicate profiles in it.",
+                     "tomvogt: ALARM" + newLine + "heinzlayla: ALARM" + newLine,
+                     outContent.toString());
+    }
+
+
+
     // ============================================================================================================================ //
     // ================================================== TEST Privacy-Functions ================================================== //
     // ============================================================================================================================ //
@@ -191,11 +277,8 @@ public class testProfile
     @Test
     public void profile_should_only_display_personal_information_that_is_public()
     {
-        //preparation for System.out.println-check
-        //compares what is printed out from "System.out.println" with an expected string
         final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
         System.setOut(new PrintStream(outContent));
-        //newLine contains the line-separator that "println()" creates after a line is printed
         String newLine = System.getProperty("line.separator");
 
 
